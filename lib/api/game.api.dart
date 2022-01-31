793d4a86f12029e3391dbbf68fd58fa0d1b51282
unknown <LOst>
2022-01-31 12:06:53 +0530

    more changes in api call

--- lib/api/game.api.dart
+++ lib/api/game.api.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import './game.dart';
+import '../models/game.dart';
 import '/models/game.dart';
 
 class GameApi {
--- lib/userdetails.dart
+++ lib/userdetails.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '/models/game.api.dart';
+import 'api/game.api.dart';
 import './models/game.dart';
 import 'controller/auth_controller.dart';
 
@@ -30,47 +30,70 @@ class _UserDetailsState extends State<UserDetails> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        automaticallyImplyLeading: false,
-        elevation: 0,
-        centerTitle: true,
-        title: const Text(
-          "Flyingwolf",
-          style: TextStyle(color: Colors.black87, fontSize: 35),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(100),
+        child: AppBar(
+          backgroundColor: Colors.white,
+          automaticallyImplyLeading: false,
+          elevation: 0,
+          centerTitle: true,
+          title: Column(
+            children: const [
+              Text(
+                "Flyingwolf",
+                style: TextStyle(color: Colors.black87, fontSize: 35),
+              ),
+              Text(
+                "Recommended for you",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
+              ),
+            ],
+          ),
+          actions: [
+            IconButton(
+                icon: const Icon(
+                  Icons.logout,
+                  color: Colors.black87,
+                ),
+                onPressed: () {
+                  AuthController.logOut();
+                })
+          ],
         ),
-        actions: [
-          IconButton(
-              icon: const Icon(
-                Icons.logout,
-                color: Colors.black87,
-              ),
-              onPressed: () {
-                AuthController.logOut();
-              })
-        ],
       ),
-      body: ListView.builder(
-        itemBuilder: (content, index) {
-          return Card(
-            child: Padding(
-              padding: const EdgeInsets.only(
-                  top: 32.0, bottom: 32.0, left: 16.0, right: 16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Image.asset("image.png"),
-                  Text(
-                    _games[index].game_name,
-                    style: const TextStyle(color: Colors.blueGrey),
-                  )
-                ],
-              ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemBuilder: (content, index) {
+                return Card(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        top: 32.0, bottom: 32.0, left: 16.0, right: 16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.network(_games[index].cover_url),
+                        SizedBox(height: 10),
+                        Text(
+                          _games[index].name,
+                          style: const TextStyle(
+                              color: Colors.black, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          _games[index].game_name,
+                          style: const TextStyle(color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+              itemCount: _games.length,
             ),
-          );
-        },
-        itemCount: _games.length,
-      ),
     );
   }
 }
